acaa21d802ebfdb3cd3c08892ad5c3e289c058da
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-06-10 11:57:22 +0200

    refactoring

--- data.py
+++ data.py
@@ -2,6 +2,35 @@ from bokeh.models import ColumnDataSource
 import numpy as np
 
 
+# Function to calculate y values
+def calculate_y_for_ewint(n, T, f):
+    if -0.01 < f < 0.01:
+        f = 0
+    w = 2 * np.pi * f
+    return np.exp(1j * w * np.arange(n) * T)
+
+
+def calculate_y_sine(n, T, f):
+    w = 2 * np.pi * f
+    return np.sin(w * np.arange(n) * T)
+
+def transfer_function(f):
+    w = 2 * np.pi * f
+    return 0.5 + 0.5 * np.exp(-1j * w)
+
+
+def magnitude_phase(H):
+    magnitude = abs(H)
+    phase = np.angle(H)
+    return magnitude, phase
+
+
+def delayed_y_for_e(n, T, f):
+    w = 2 * np.pi * f
+    calculate_y = calculate_y_for_ewint(n, T, f)
+    return 0.5 * calculate_y + 0.5 * calculate_y * np.exp(-1j * w)
+
+
 class Data:
     def __init__(self):
         self.f = 1
@@ -21,11 +50,11 @@ class Data:
 
     def update_data(self):
         # Function to calculate y values
-        y = self.calculate_y_for_ewint(self.N, self.T, self.f)
+        y = calculate_y_for_ewint(self.N, self.T, self.f)
 
         # Function to calculate transfer function
-        H = self.transfer_function(self.f)
-        magnitude, phase = self.magnitude_phase(H)
+        H = transfer_function(self.f)
+        magnitude, phase = magnitude_phase(H)
         self.magnitude_and_phase.data = dict(x=[0, magnitude * np.cos(phase)], y=[0, magnitude * np.sin(phase)])
 
         # Update the y(n) plot
@@ -35,36 +64,9 @@ class Data:
         self.complex_original_signal.data = dict(x=np.real(y), y=np.imag(y))
 
         # Update the delayed y(n) plot
-        delayed_y = self.delayed_y_for_ewint(self.N, self.T, self.f)
+        delayed_y = delayed_y_for_e(self.N, self.T, self.f)
         self.delayed_signal.data = dict(x=np.arange(self.N), y=np.real(delayed_y))
 
         # Update the sine plot
-        sine_y = self.calculate_y_sine(self.N, self.T, self.f)
+        sine_y = calculate_y_sine(self.N, self.T, self.f)
         self.sine_original.data = dict(x=np.arange(self.N), y=sine_y)
-
-    # Function to calculate y values
-    def calculate_y_for_ewint(self, n, T, f):
-        if -0.01 < f < 0.01:
-            f = 0
-        w = 2 * np.pi * f
-        return np.exp(1j * w * np.arange(n) * T)
-
-    def delayed_y_for_ewint(self, n, T, f):
-        w = 2 * np.pi * f
-        calculate_y = self.calculate_y_for_ewint(n, self.T, self.f)
-        return 0.5 * calculate_y + 0.5 * calculate_y * np.exp(-1j * w)
-
-    def calculate_y_sine(self, n, T, f):
-        w = 2 * np.pi * f
-        return np.sin(w * np.arange(n) * T)
-
-    # Function to calculate transfer function
-    def transfer_function(self, f):
-        w = 2 * np.pi * f
-        return 0.5 + 0.5 * np.exp(-1j * w)
-
-    # Function to calculate magnitude and phase from a complex number
-    def magnitude_phase(self, H):
-        magnitude = abs(H)
-        phase = np.angle(H)
-        return magnitude, phase
