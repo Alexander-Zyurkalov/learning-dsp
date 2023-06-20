1cc4220dd4082b95fc6d32a3be580c9294801a63
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-06-20 13:08:06 +0200

    rearranging the diagrams

--- dsp.py
+++ dsp.py
@@ -9,41 +9,50 @@ from sliders import FrequencySlider, SamplesSlider, SamplingFrequencySlider, Tim
 data = Data()
 
 # Create a new plot with a single line (the 'hand')
-p1 = figure(width=400, height=400, x_range=(-1, 1), y_range=(-1, 1), title='Magnitude and Phase')
-p1.line('x', 'y', line_width=2, source=data.magnitude_and_phase)
+hand = figure(width=400, height=400, x_range=(-1, 1), y_range=(-1, 1), title='Magnitude and Phase')
+hand.line('x', 'y', line_width=2, source=data.magnitude_and_phase)
 
-p3 = figure(width=400, height=400, x_range=(-1.2, 1.2), y_range=(-1.2, 1.2), title='Complex y(n)')
-p3.line('x', 'y', line_width=2, source=data.complex_original_signal)
+compl = figure(width=400, height=400, x_range=(-1.2, 1.2), y_range=(-1.2, 1.2), title='Complex y(n)')
+compl.line('x', 'y', line_width=2, source=data.complex_original_signal)
+
+# Create the new plots
+p1 = figure(width=1500, height=400,  y_range=(-1.2, 1.2), title='Plot 1')
+p2 = figure(width=1500, height=400,  y_range=(-1.2, 1.2), title='Plot 2')
 
 colors = ['red', 'green', 'blue', 'indigo', 'orange', 'yellow']
 
-# Create the figures in a loop
-figure_list = []
+checkbox_columns = [[], []]  # separate checkbox list for each figure
 
+# Iterate over the signal groups
 for group_name, signals in data.signal_groups.items():
-    checkbox_list = []
-    p = figure(width=1500, height=400, title=group_name, y_range=(-1.2, 1.2))
+
+    # Iterate over the signals in each group
     for index, (signal_name, source) in enumerate(signals.items()):
-        # Pick a color from the list, cycling back to the start if there are more signals than colors
+        # Pick a color from the list
         color = colors[index % len(colors)]
-        line = p.line('x', 'y', source=source, color=color, legend_label=signal_name)
 
-        # Add a checkbox for each line
-        checkbox = CheckboxGroup(labels=[signal_name], active=[0], width=150)
+        # Add a line for each signal on each plot
+        line1 = p1.line('x', 'y', source=source, color=color, legend_label=signal_name)
+        line2 = p2.line('x', 'y', source=source, color=color, legend_label=signal_name)
 
-        # Attach a callback to each checkbox to control the visibility of the corresponding line
-        callback = CustomJS(args=dict(line=line, checkbox=checkbox), code="""
+        # Add a checkbox for each line in each figure
+        checkbox1 = CheckboxGroup(labels=[signal_name], active=[0], width=150)
+        checkbox2 = CheckboxGroup(labels=[signal_name], active=[0], width=150)
+
+        # Attach a callback to each checkbox to control the visibility of the corresponding lines
+        callback1 = CustomJS(args=dict(line=line1, checkbox=checkbox1), code="""
             line.visible = checkbox.active.includes(0);
         """)
-        checkbox.js_on_change('active', callback)
+        checkbox1.js_on_change('active', callback1)
+
+        callback2 = CustomJS(args=dict(line=line2, checkbox=checkbox2), code="""
+            line.visible = checkbox.active.includes(0);
+        """)
+        checkbox2.js_on_change('active', callback2)
 
         # Add the checkbox to the list
-        checkbox_list.append(checkbox)
-
-    # Arrange each plot and its checkboxes in a column and add to the figure_list
-    figure_list.append(column(p, *checkbox_list))
-
-
+        checkbox_columns[0].append(checkbox1)
+        checkbox_columns[1].append(checkbox2)
 
 # Create sliders
 frequency_slider = FrequencySlider(data, start=-5, end=20, value=data.f, step=0.005, title="f")
@@ -56,15 +65,26 @@ select_nyquist = ParameterSelectNyquist(data, frequency_slider)
 sampling_frequency_slider.add_select(select_nyquist)
 frequency_slider.add_select(select_nyquist)
 
-# Arrange the plot and the slider in a column
-layout1 = column(p1, p3)
-layout2 = column(
-    samples_slider.slider, sampling_frequency_slider.slider, time_slider.slider,
-    *figure_list)
+# Arrange the plots, the slider and the checkboxes in a layout
+layout = column(
+    frequency_slider.slider,
+    row(
+        column(
+            samples_slider.slider,
+            time_slider.slider,
+            sampling_frequency_slider.slider,
+            select_nyquist.select,
+        ),
+        row(hand, compl)
+    ),
+    column(
+        row(column(*checkbox_columns[0])),
+        p1,
+        row(column(*checkbox_columns[1])),
+        p2
+    ),
+)
 
+# Update data and add root
 data.update_data()
-
-layout = row(layout1, layout2)
-curdoc().add_root(select_nyquist.select)
-curdoc().add_root(frequency_slider.slider)
 curdoc().add_root(layout)
